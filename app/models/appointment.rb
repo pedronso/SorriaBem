1499d1b3a9872f91c0cf0cb5de18818a16ff9561
filipegomes11 <filipeandradegomes@gmail.com>
2023-07-27 21:34:48 -0300

    Adicionando atributos e validates para appointment (consulta).

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,25 @@
 class Appointment < ApplicationRecord
+  belongs_to :dentist
+
+  attribute :date, :date
+  attribute :time, :time
+
+  validates :date, presence: true
+  validates :time, presence: true
+  validate :date_not_in_past
+  validate :valid_date
+
+  private
+
+  def date_not_in_past
+    if date.present? && date < Date.today
+      errors.add(:date, "não pode ser marcada no passado")
+    end
+  end
+
+  def valid_date
+    if date.present? && time.present? && DateTime.parse("#{date} #{time}") < DateTime.now
+      errors.add(:date, "horário já passou")
+    end
+  end
 end
