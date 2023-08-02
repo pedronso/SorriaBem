1c904523834af6199e89810e890611111fe359a3
Neto_SK9 <netobatista090@gmail.com>
2023-08-02 00:32:53 -0300

    Criação dos três testes de Dentists.

--- test/models/dentist_test.rb
+++ test/models/dentist_test.rb
@@ -1,7 +1,31 @@
-require "test_helper"
+require_relative "../test_helper"
 
 class DentistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @dentist = Dentist.new(
+      nome: "Dentist One",
+      cpf: "12345678901",
+      email: "dentist@example.com",
+      especialidade: "Odontologia Geral",
+      cro: "12345",
+      inicio_horario_atendimento: "08:00:00",
+      termino_horario_atendimento: "17:00:00"
+    )
+  end
+
+  test "should not save dentist without name" do
+    @dentist.nome = nil
+    assert_not @dentist.save, "Saved the dentist without a name"
+  end
+
+  test "should not save dentist with invalid CPF format" do
+    @dentist.cpf = "12345"
+    assert_not @dentist.save, "Saved the dentist with an invalid CPF format"
+  end
+
+  test "should not save dentist with end time before start time" do
+    @dentist.inicio_horario_atendimento = "17:00:00"
+    @dentist.termino_horario_atendimento = "08:00:00"
+    assert_not @dentist.save, "Saved the dentist with end time before start time"
+  end
 end
