c9b4e9851ec1f7b337d57e95e093b9f2f3d33ad3
Benjamin <86573978+bennie10colado@users.noreply.github.com>
2023-08-02 12:51:34 -0300

    Adicionando a validação de não existir varias consultas para o mesmo médico ao mesmo tempo, resultando em um conflito. Testes realizados com sucesso

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -28,9 +28,21 @@ class AppointmentTest < ActiveSupport::TestCase
     assert_not @appointment.save, "Saved the appointment without a date"
   end
 
+  test "should not save appointment without a time" do
+    @appointment.time = nil
+    assert_not @appointment.save, "Saved the appointment without a time"
+  end
+
   test "should not save appointment with a past time" do
     @appointment.date = Date.yesterday
     @appointment.time = "12:00:00"
     assert_not @appointment.save, "Saved the appointment with a past time"
+  end
+
+  test "should not allow conflicting appointments" do
+    existing_appointment = Appointment.create(date: Date.tomorrow, time: "14:30:00", dentist: @dentist)
+    conflicting_appointment = Appointment.new(date: Date.tomorrow, time: "14:30:00", dentist: @dentist)
+
+    assert_not conflicting_appointment.save, "Saved a conflicting appointment"
   end
 end
