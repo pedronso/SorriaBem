589bf3531b85e2fed3eee6cf010251831b31e9dc
TayaneCibely <tayanerodrigues2001@gmail.com>
2023-08-03 14:37:18 -0300

    codacy: importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require @hotwired/turbo-rails
 import "@hotwired/turbo-rails";
 import "controllers";
 
