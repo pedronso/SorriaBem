5a779aed1b29da5a6fa91227319ae8e8f3c15792
filipegomes11 <filipeandradegomes@gmail.com>
2023-08-09 22:27:54 -0300

    CRUD de Paciente feito, bem como adequação ao design do site.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -19,18 +19,19 @@ class PatientsController < ApplicationController
   def edit
   end
 
+
+  def search
+    @patients = Patient.where("full_name LIKE ?", "%#{params[:query]}%")
+  end
+
   # POST /patients or /patients.json
   def create
     @patient = Patient.new(patient_params)
 
-    respond_to do |format|
-      if @patient.save
-        format.html { redirect_to patient_url(@patient), notice: "Patient was successfully created." }
-        format.json { render :show, status: :created, location: @patient }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @patient.errors, status: :unprocessable_entity }
-      end
+    if @patient.save
+      redirect_to patients_url, notice: "Patient was successfully created."
+    else
+      render :new
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,14 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :patients do
+    collection do
+      get 'search'
+    end
+  end
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
